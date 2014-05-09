418a9269c04b075cc7d90011227fb3a0842eb353
Jesse Panganiban <me@jpanganiban.com>
2014-05-10 01:35:40 +0800

    Make grid dynamic

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,3 +1,14 @@
 class Inquiry < ActiveRecord::Base
   attr_accessible :email, :lat, :lon, :size
+
+  @@grid = [
+    (1..6).to_a,
+    (7..12).to_a,
+    (13..18).to_a,
+    (19..24).to_a,
+  ]
+
+  def self.grid
+    @@grid
+  end
 end

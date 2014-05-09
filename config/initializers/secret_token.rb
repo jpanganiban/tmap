5226098c34ab062a75a38a0e555d6e1fcbb17ede
Jesse Panganiban <me@jpanganiban.com>
2014-05-09 22:36:37 +0800

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmap::Application.config.secret_token = 'fc6dbaf3783b17f35e16f214bea96bef26f4846ff3de504f2fbc572f768c58a9a2454b40cb509eab210c6dbab73fe07dfc5ced3c0d5a3509809920d2535b9478'

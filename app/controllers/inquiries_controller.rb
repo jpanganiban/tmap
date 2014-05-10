debe2f0a91c6468c08e47e181d87cc3e6ebe6286
Ardee Aram <ardee.aram@galoretv.com>
2014-05-10 09:00:11 +0800

    /inquiries/:id/result with random result

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -13,4 +13,25 @@ class InquiriesController < ApplicationController
     # TODO: Redirect to page 3
     redirect_to :root
   end
+  
+  def result
+  	begin
+  		@inquiry = Inquiry.find(params[:id])
+  	rescue ActiveRecord::RecordNotFound => ex
+  		@safety_display = "Not found."
+  	end
+  	random_response = Random.rand(3)
+
+	case random_response
+	when 0
+		@safety_display = "Yes."
+	when 1
+		@safety_display = "No."
+	else
+		@safety_display = "Maybe."
+	end  	
+  	
+  	
+  end
+
 end
